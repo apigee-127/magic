09c4c97b6523c60ee51a328f24cadf23be3f7020
Scott Ganyo <scott@ganyo.com>
2014-09-17 17:32:21 -0700

    fix test

--- test/middleware.js
+++ test/middleware.js
@@ -1,7 +1,6 @@
 var should = require('should');
 var path = require('path');
 var a127config = require('../lib/config');
-var middleware = require('../lib/middleware');
 
 var CONFIG_DIR = path.resolve(__dirname, 'config');
 process.env.A127_CONFIG = CONFIG_DIR;
@@ -12,6 +11,8 @@ var config = a127config.load();
 
 config['a127.magic'].swaggerFile = SWAGGER_FILE;
 config['a127.magic'].controllers = {};
+
+var middleware = require('../lib/middleware');
 
 describe('middleware', function() {
 
