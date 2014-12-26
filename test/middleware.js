cf403e7e4e8cebe7954822ce2f2d0d188473899a
Scott Ganyo <scott@ganyo.com>
2014-12-26 09:57:06 -0800

    Check config for "validateReponse" option - if true, pass to swaggerValidator. closes #5

--- test/middleware.js
+++ test/middleware.js
@@ -3,6 +3,7 @@ var a127config = require('../lib/config');
 process.env.A127_APPROOT = __dirname;
 
 var middleware = require('../lib/middleware');
+var should = require('should');
 
 describe('middleware', function() {
 
@@ -15,7 +16,17 @@ describe('middleware', function() {
   });
 
   it('must load correctly', function(done) {
+
+    config.validateResponse.should.be.true;
+
     middleware(config);
+
+    var magic = config['a127.magic'];
+    should.exist(magic);
+
+    var swaggerTools = magic.swaggerTools;
+    should.exist(swaggerTools);
+
     done();
   });
 });
